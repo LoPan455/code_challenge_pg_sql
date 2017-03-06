68cdb696b272925c67dc4540ce01b831c6f8d15a
Lopan455 <tjohander@gmail.com>
2017-03-06 14:42:53 -0600

    GET route to /treats returning data

--- server/app.js
+++ server/app.js
@@ -10,22 +10,45 @@ var config = {
   max: 10, // how many connections at one time
   idleTimeoutMillis: 30000 // 30 seconds to try to connect
 };
+var pool = new pg.Pool(config);
+var bodyParser = require ('body-parser')
+
 
 /*** Build out a module to manage our treats requests. ***/
 
 app.get('/treats',function(req,res){
   console.log('app.get to /treats reached');
-  res.sendStatus(200);
-}) // app.get to /getTreats
+  pool.connect(function(errorConnectingToDatabase, client, done){
+    if(errorConnectingToDatabase) {
+      console.log('Error connecting to database: ', errorConnectingToDatabase);
+      res.sendStatus(500);
+    } else {
+      client.query('SELECT * FROM treats;', function(errorMakingQuery, result){
+        done();
+        if(errorMakingQuery) {
+          console.log('Error making the database query: ', errorMakingQuery);
+          res.sendStatus(500);
+        } else {
+          res.send(result.rows);
+        }
+      });
+    }
+  });
+});
+// end app.get to /getTreats
 
 app.post('/treats',function(req,res){
   console.log('app.post to /treats reached');
   res.sendStatus(200);
-})
+}) // end app.post to /treats
 
 
 // Get static files
 app.use(express.static('./server/public'));
+
+// use body-parser
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Get index.html
 app.get('/', function(req, res) {
